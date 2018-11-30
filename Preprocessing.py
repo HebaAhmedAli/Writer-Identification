e40a86a9c24362f8b826dd886ceddf9b85d477e4
HebaAhmedAli <bobaahmed2001@gmail.com>
2018-11-30 14:58:22 -0500

    Adding conversion to gray

--- Preprocessing.py
+++ Preprocessing.py
@@ -3,6 +3,6 @@ import cv2
 def processImage(image):
     value = (5, 5)
     blurred = cv2.GaussianBlur(image, value, 0)
-    grayImage = cv2.cvtColor(blurred,cv2.COLOR_RGB2GRAY)
+    grayImage = cv2.cvtColor(blurred,cv2.COLOR_BGR2GRAY)
     ret,binarized= cv2.threshold(grayImage,175,255,cv2.THRESH_BINARY)
     return binarized
