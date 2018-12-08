d3fea3ca9b54e19214a0fb969d54ab2a936e4ec1
FeryalHisham <fargalaxy24@gmail.com>
2018-12-08 05:21:02 +0200

    Add edge direction and hinge features

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
 from DataSet import dataSet as dataSet
 import FeatureExtraction as featureExtraction
 from features.CO3 import co3 as co3
+import features.EdgeHinge as hinge
 import PerformanceEvaluation as performanceEvaluation
 import KnnIdentification as knnIdentification
 import time
+import numpy as np
 
 
 start = time.time()
@@ -12,27 +14,24 @@ start = time.time()
 # Load the dataset and devide it between training and testing and make the nedded preprocessing.
 dataSet.loadDataset()
 print("Time taken to loadDataset = "+str(time.time() - start))
-
 sartExtractFeatures=time.time()
 # Extract the featureVectors for all the writers may need additional preprocessing inside.
-classifiedCO3,featureVectors=featureExtraction.extractFeatures("co3",dataSet.trainingDataImages)
+classifiedCO3,featureVectors=featureExtraction.extractFeatures("edgeHinge",dataSet.trainingDataImages)
 #print(featureVectors) # TODO: Delete after testing
 print("Time taken to extractFeatures = "+str(time.time() - sartExtractFeatures))
 
 # Evaluate the performance on the training data.
 sartEvaluatePerformance=time.time()
-performanceEvaluation.evaluatePerformance("co3",dataSet.testingDataImages,dataSet.testingDataWriters,featureVectors,dataSet.tariningDataWriters,classifiedCO3)
+performanceEvaluation.evaluatePerformance("edgeHinge",dataSet.testingDataImages,dataSet.testingDataWriters,featureVectors,dataSet.tariningDataWriters,classifiedCO3)
 print("Time taken to evaluatePerformance = "+str(time.time() - sartEvaluatePerformance))
 
 
 # Identify any writer.
 sartIdentifyWriter=time.time()
 writerImages=knnIdentification.readWriterImages(1)
-writerId=knnIdentification.identifyWriter("co3",featureVectors,dataSet.tariningDataWriters,writerImages[0],classifiedCO3)
+writerId=knnIdentification.identifyWriter("edgeHinge",featureVectors,dataSet.tariningDataWriters,writerImages[0],classifiedCO3)
 print("This image belongs to writer = "+str(writerId))
 print("Time taken to identifyWriter = "+str(time.time() - sartIdentifyWriter))
 
 # Printing the time taken.
 print("Time taken to excute the code = "+str(time.time() - start))
-
-
